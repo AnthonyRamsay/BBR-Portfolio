b23c22669846a130796c609fec6523d1ddc40f40
builtbyr <96205031+AnthonyRamsay@users.noreply.github.com>
2024-11-20 23:09:00 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,23 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
         updateSlide(currentSlide);
     }
 
-    // Function to show the previous slide
-    function showPreviousSlide() {
-        currentSlide = (currentSlide - 1 + slides.length) % slides.length; // Loop to the last slide
-        updateSlide(currentSlide);
-    }
-
     // Automatic slideshow every 12 seconds
     setInterval(showNextSlide, 12000);
-
-    // Keyboard navigation
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowRight') {
-            showNextSlide();
-        } else if (event.key === 'ArrowLeft') {
-            showPreviousSlide();
-        }
-    });
 
     // Initialize the first slide
     updateSlide(currentSlide);
